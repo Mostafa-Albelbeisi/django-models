efd213369bb053a173dbe7e28a96c54e9fe96940
Mostafa-Albelbeisi <mostfabelbeisi@gmail.com>
2022-12-03 16:53:48 +0000

    feat: add details templates

--- snacks/views.py
+++ snacks/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
-from django .views.generic import TemplateView, ListView
+from django.views.generic import TemplateView,ListView,DetailView
 from .models import Snack
 
-# Create your views here.
+class HomePage(TemplateView):
+    template_name='home.html'
 
+class SnackListView(ListView):
+    template_name='snack_list.html'
+    model=Snack
 
-class Home(TemplateView):
-    template_name = 'home.html'
-
-class SnackDetail(ListView):
-    template_name = 'snack_detail.html'
-    model = Snack
-
-class SnackList(ListView):
-    template_name = 'snack_list.html'
-    model = Snack
-
-
-class About(TemplateView):
-    template_name = 'about.html'
+class SnackDetail(DetailView):
+    template_name='snack_detail.html'
+    model=Snack
